f0bd5df276068e10275fd8a78632f638cdf084b2
Yoana Naydenova <y.naydenova@gmail.com>
2024-08-25 10:18:43 +0300

    create initial project

---
+++ src/main/java/com/naydenova/pharmacy_items/PharmacyItemsApplication.java
@@ -0,0 +1,12 @@
+package com.naydenova.pharmacy_items;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyItemsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/naydenova/pharmacy_items/PharmacyItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naydenova.pharmacy_items;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
