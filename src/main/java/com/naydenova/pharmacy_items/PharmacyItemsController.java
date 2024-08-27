f90a2ee9941e6393cc04402979526124d24979cd
Yoana Naydenova <y.naydenova@gmail.com>
2024-08-27 21:56:13 +0300

    refactor PharmacyItemsController

--- src/main/java/com/naydenova/pharmacy_items/PharmacyItemsController.java
+++ src/main/java/com/naydenova/pharmacy_items/PharmacyItemsController.java
@@ -1,5 +1,8 @@
 package com.naydenova.pharmacy_items;
 
+import com.naydenova.pharmacy_items.service.PharmacyService;
+import com.naydenova.pharmacy_items.service.SopharmacyPharmacyService;
+import com.naydenova.pharmacy_items.service.SubraPharmacyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -7,23 +10,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin("http://localhost:5173")
 public class PharmacyItemsController {
 
-    private final SopharmacyService service;
+    private final SopharmacyPharmacyService sopharmacyPharmacyService;
+    private final SubraPharmacyService subraPharmacyService;
+
 
     @Autowired
-    public PharmacyItemsController(SopharmacyService service) {
-        this.service = service;
+    public PharmacyItemsController(SopharmacyPharmacyService sopharmacyPharmacyService, SubraPharmacyService subraPharmacyService) {
+        this.sopharmacyPharmacyService = sopharmacyPharmacyService;
+        this.subraPharmacyService = subraPharmacyService;
     }
 
-    @GetMapping("/items/{itemName}")
-    private ResponseEntity<List<Item>> findAll(@PathVariable String itemName) {
-        List<Item> items = service.findItems(itemName);
+
+    @GetMapping("/items")
+    private ResponseEntity<List<Item>> findAll(@RequestParam List<Integer> pharms, @RequestParam String text) {
+        final List<Item> items = pharms.stream()
+                .map(pharmKey -> getService(pharmKey).parseItems(text)).flatMap(Collection::stream).collect(Collectors.toList());
         return ResponseEntity.ok(items);
     }
+
+
+    private  PharmacyService getService(Integer pharmKey) {
+        return switch (pharmKey){
+            case 1 ->sopharmacyPharmacyService;
+            case 2 -> subraPharmacyService;
+            default -> throw new RuntimeException("This pharmacy is no supported!");
+        };
+
+    }
+
 
 }
