d5c8bf57bdb5cc6c57cfc5dfdf0c84e492f077a0
Yoana Naydenova <y.naydenova@gmail.com>
2024-09-19 20:23:00 +0300

    fix path in PharmacyItemsController

--- src/main/java/com/naydenova/pharmacy_items/controllers/PharmacyItemsController.java
+++ src/main/java/com/naydenova/pharmacy_items/controllers/PharmacyItemsController.java
@@ -1,12 +1,12 @@
-package com.naydenova.pharmacy_items;
+package com.naydenova.pharmacy_items.controllers;
 
-import com.naydenova.pharmacy_items.service.PharmacyService;
-import com.naydenova.pharmacy_items.service.RemediumPharmacyService;
-import com.naydenova.pharmacy_items.service.SopharmacyPharmacyService;
-import com.naydenova.pharmacy_items.service.SubraPharmacyService;
+import com.naydenova.pharmacy_items.dtos.ItemDto;
+import com.naydenova.pharmacy_items.services.PharmacyService;
+import com.naydenova.pharmacy_items.services.RemediumPharmacyService;
+import com.naydenova.pharmacy_items.services.SopharmacyPharmacyService;
+import com.naydenova.pharmacy_items.services.SubraPharmacyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
@@ -17,12 +17,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin("http://localhost:5173")
+//@CrossOrigin(origins = "http://localhost:5173")
 public class PharmacyItemsController {
 
     private final SopharmacyPharmacyService sopharmacyPharmacyService;
     private final SubraPharmacyService subraPharmacyService;
-
     private final RemediumPharmacyService remediumPharmacyService;
 
 
@@ -33,23 +32,21 @@ public class PharmacyItemsController {
         this.remediumPharmacyService = remediumPharmacyService;
     }
 
-
-    @GetMapping("/items")
-    private ResponseEntity<List<Item>> findAll(@RequestParam List<Integer> pharms, @RequestParam long limit,@RequestParam String text) {
-        final List<Item> items = pharms.parallelStream()
+    @GetMapping("/search")
+    private ResponseEntity<List<ItemDto>> findAll(@RequestParam List<Integer> pharms, @RequestParam long limit, @RequestParam String text) {
+        final List<ItemDto> items = pharms.parallelStream()
                 .map(pharmKey -> getService(pharmKey)
                         .setLimit(limit).parseItems(text))
                 .flatMap(Collection::stream)
-                .sorted(Comparator.comparing(Item::price, Comparator.nullsLast(Comparator.naturalOrder())))
+                .sorted(Comparator.comparing(ItemDto::getPrice, Comparator.nullsLast(Comparator.naturalOrder())))
                 .collect(Collectors.toList());
 
         return ResponseEntity.ok(items);
     }
 
-
-    private  PharmacyService getService(Integer pharmKey) {
-        return switch (pharmKey){
-            case 1 ->sopharmacyPharmacyService;
+    private PharmacyService getService(Integer pharmKey) {
+        return switch (pharmKey) {
+            case 1 -> sopharmacyPharmacyService;
             case 2 -> subraPharmacyService;
             case 3 -> remediumPharmacyService;
             default -> throw new RuntimeException("This pharmacy is no supported!");
