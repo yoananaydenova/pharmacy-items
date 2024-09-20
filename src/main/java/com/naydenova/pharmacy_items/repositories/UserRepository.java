2f0172e295c476207dbc64683fa3c926f8fdf2bc
Yoana Naydenova <y.naydenova@gmail.com>
2024-09-20 10:42:54 +0300

    add UserRepository

---
+++ src/main/java/com/naydenova/pharmacy_items/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.naydenova.pharmacy_items.repositories;
+
+
+
+
+import com.naydenova.pharmacy_items.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByLogin(String login);
+}
