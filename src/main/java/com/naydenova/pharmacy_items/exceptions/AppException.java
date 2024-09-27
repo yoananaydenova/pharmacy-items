73f253fdf15f6631e762448fd114377046e06a6c
Yoana Naydenova <y.naydenova@gmail.com>
2024-09-27 19:49:43 +0300

    add AppException

---
+++ src/main/java/com/naydenova/pharmacy_items/exceptions/AppException.java
@@ -0,0 +1,17 @@
+package com.naydenova.pharmacy_items.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class AppException extends RuntimeException {
+
+    private final HttpStatus status;
+
+    public AppException(String message, HttpStatus status) {
+        super(message);
+        this.status = status;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
