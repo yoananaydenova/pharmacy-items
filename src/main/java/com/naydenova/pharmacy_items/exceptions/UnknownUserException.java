447dca08d4d5c645a7834fd9ec86fc95716e1a41
Yoana Naydenova <y.naydenova@gmail.com>
2025-01-24 13:36:53 +0200

    fix UnknownUserException

--- src/main/java/com/naydenova/pharmacy_items/exceptions/UnknownUserException.java
+++ src/main/java/com/naydenova/pharmacy_items/exceptions/UnknownUserException.java
@@ -1,7 +1,16 @@
 package com.naydenova.pharmacy_items.exceptions;
 
+import org.springframework.http.HttpStatus;
+
 public class UnknownUserException extends RuntimeException{
-    public UnknownUserException(){
+
+    private final HttpStatus status = HttpStatus.NOT_FOUND;
+
+    public UnknownUserException() {
         super("Unknown user!");
+    }
+
+    public HttpStatus getStatus() {
+        return status;
     }
 }
