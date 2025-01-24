4e5d1721a4a5ba3ace7b24cbaf9dde031fbdd16f
Yoana Naydenova <y.naydenova@gmail.com>
2025-01-24 13:25:09 +0200

    replace env variables

--- src/main/java/com/naydenova/pharmacy_items/config/WebConfig.java
+++ src/main/java/com/naydenova/pharmacy_items/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.naydenova.pharmacy_items.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,12 +20,15 @@ public class WebConfig {
     private static final Long MAX_AGE = 3600L;
     private static final int CORS_FILTER_ORDER = -102;
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Bean
     public FilterRegistrationBean corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin(frontendUrl);
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
